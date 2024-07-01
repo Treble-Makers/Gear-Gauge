d50c855a843f9a032e35fe7dd6a3988ddccee9e1
KatieNFerg <ketchersidekatie@gmail.com>
2024-07-01 12:33:39 -0500

    feature-created musicItems Model/Controller-kf

---
+++ GearGauge/Controllers/Music_ItemController.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GearGauge.Controllers;
+
+public class Music_Item : Controller
+{
+
+}
---
+++ GearGauge/Models/Music_Item.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Components.Web;
+
+
+namespace GearGauge.Models;
+
+public class Music_Item
+{
+    public int Id {get; }
+    static private int nextId = 1;
+    public string? Title {get; set; }
+    public CommentId commentId{get; set; }
+
+    public CategoryId categoryId {get; set; }
+
+
+}
+
+public MusicItem()
+{
+    Id = nextId;
+    nextId++;
+}
+
+public MusicItem()
+{
+    Title = title;
+    CommentId = commentId;
+    CategoryId = categoryId;
+}
+
+public override string ToString()
+{
+    string output = "";
+    if(Title.Equals(""))
+    {
+        PageTitle = "Data not available";
+    }
+
+    output = string.Format("\nID: %d\n" + "Title: %s\n", Id, Title);
+    return output;
+}
+
+public override bool Equals(object obj)
+{
+    return obj is MusicItem musicItem &&
+    Id == musicItem.Id;
+}
+
+public override int GetHashCode()
+{
+    return HashCode.Combine(Id);
+}
