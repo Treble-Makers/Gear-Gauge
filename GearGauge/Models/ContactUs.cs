1843d6d767899b4747685760be41ff32ea2eaa90
KatieNFerg <ketchersidekatie@gmail.com>
2024-07-02 14:45:28 -0500

    feature - started model - kf

--- GearGauge/Models/ContactUs.cs
+++ GearGauge/Models/ContactUs.cs
@@ -4,6 +4,14 @@ namespace GearGauge.Models;
 
 public class ContactUs
 {
-    public string
+    public string? UserName { get; set; }
+    public string? ContactEmail { get; set; }
+    private  readonly string? GearEmail = "geargauge@hotmail.com";
+    
+
+public ContactUs(string userName, string contactEmail)
+{
+    UserName = userName;
+    ContactEmail = contactEmail;
 
 }
