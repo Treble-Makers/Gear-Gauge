381912501f55bbfc765515651eb6e733b9c6d65d
DallasJB <149632069+DallasJB@users.noreply.github.com>
2024-07-03 10:46:50 -0500

    Thank you page- View HTML added

--- GearGauge/Controllers/HomeController.cs
+++ GearGauge/Controllers/HomeController.cs
@@ -23,6 +23,12 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult ThankYou(string message)
+    {
+        ViewBag.Message = message;
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
