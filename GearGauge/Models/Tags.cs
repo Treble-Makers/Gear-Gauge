1fa8614873a8efec7fed857c54dd9b13d29b0bb3
DallasJB <149632069+DallasJB@users.noreply.github.com>
2024-07-10 18:32:22 -0500

    data base work

--- GearGauge/Models/Tags.cs
+++ GearGauge/Models/Tags.cs
@@ -6,7 +6,7 @@ namespace GearGauge.Models
     public class Tag
     {
         [Key]
-        public int Id { get; set; } // This is the Id property that is missing
+        public int Id { get; set; }
 
         [Required]
         public string Name { get; set; }
