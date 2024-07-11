43229abc89bd69c2d122b3b805214abca46d817d
DallasJB <149632069+DallasJB@users.noreply.github.com>
2024-07-11 09:38:18 -0500

    push stuff, not 100% sure I trust

--- GearGauge/Controllers/GearInventoryController.cs
+++ GearGauge/Controllers/GearInventoryController.cs
@@ -63,6 +63,8 @@ namespace GearGauge.Controllers
                         addGearInventoryViewModel.ImageFile.CopyTo(fileStream);
                     }
                 }
+                    Console.Write(uniqueFileName, "\n\n\n");
+                    
 
                 GearInventory newGearInventory = new GearInventory
                 {
