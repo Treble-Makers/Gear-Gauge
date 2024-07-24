83e5c6a1d9c56d0d7392e6579748c396f67b7151
Guest <StoppingBy@Nahomis-MacBook-Pro-2.local>
2024-07-24 15:59:07 -0500

    fixed comments errors

---
+++ GearGauge/Controllers/ForumController.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using GearGauge.Data;
+using GearGauge.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GearGauge.Controllers
+{
+    [Authorize]
+    public class ForumController : Controller
+    {
+        private readonly GearGaugeDbContext _context;
+        private readonly UserManager<User> _userManager;
+
+        public ForumController(GearGaugeDbContext context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var comments = await _context.Comments
+                .Include(c => c.User)
+                .Include(c => c.Replies)
+                    .ThenInclude(r => r.User)
+                .Where(c => c.ParentCommentId == null)
+                .OrderByDescending(c => c.CreatedAt)
+                .ToListAsync();
+
+            return View(comments);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostComment(string content, int? parentCommentId)
+        {
+            if (string.IsNullOrEmpty(content))
+            {
+                return BadRequest("Comment content cannot be empty.");
+            }
+
+            var user = await _userManager.GetUserAsync(User);
+            var comment = new Comment
+            {
+                Content = content,
+                CreatedAt = DateTime.UtcNow,
+                UserId = user.Id,
+                ParentCommentId = parentCommentId
+            };
+
+            _context.Comments.Add(comment);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
