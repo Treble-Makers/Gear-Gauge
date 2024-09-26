5182b4ab7b04b2b15c85deadff6955d428d9ac3f
TellyBellyJellyBean <pokers-02erasers@icloud.com>
2024-09-26 15:03:58 -0500

    Enhance

--- GearGauge/Models/Search/VideoDetailsResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GearGauge.Models;
-
-public class VideoDetailsResponse
-{
-    public List<VideoDetails> Videos { get; set; } = new List<VideoDetails>();
-    // public string? NextPageToken { get; set; }
-    // public string? PrevPageToken { get; set; }
-}
